226c1251aa3ad6e9f1227ab2028ee230b90b4da1
Vinícius Henrique de Oliveira <63620777+Vini28498@users.noreply.github.com>
2023-02-16 09:41:09 -0300

    Add files via upload

---
+++ convert_parquet.py
@@ -0,0 +1,24 @@
+# Importar biblioteca para trabalhar dentro do diretório
+import os 
+
+# Entrando dentro do diretório
+diretorio = r"C:/Users/vinicius.oliveira/Desktop/Teste/"
+
+# Percorrendo o diretório
+result = [f for f in os.listdir(diretorio) if os.path.isfile(os.path.join(diretorio, f))]
+print(result)
+
+# Importando biblioteca de transformação de dados
+import pandas as pd
+
+# Importando bliblioteca para trabalhar com arquivos parquet
+import pyarrow
+
+# Tranformando os arquivos\s
+for arquivo in result:
+    df = pd.read_parquet(diretorio + arquivo)
+    csv_out = r'C:/Users/vinicius.oliveira/Desktop/Teste/'+ arquivo[:-8] + '.csv'
+    output = df.to_csv(csv_out, sep=';', index = False)
+
+print('Tranformação concluída com sucesso')
+
